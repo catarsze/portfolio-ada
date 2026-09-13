d63dab602c43406d91491f81ab02cfe2ea22b27d
𝑽 <137126119+catarsze@users.noreply.github.com>
2026-09-12 23:08:33 -0300

    Create leaflet-providers.js

---
+++ scripts/leaflet-providers.js
@@ -0,0 +1,4 @@
+<head>
+  <script src="https://unpkg.com/leaflet@latest/dist/leaflet.js"></script>
+  <script src="https://unpkg.com/leaflet-providers@latest/leaflet-providers.js"></script>
+</head>
